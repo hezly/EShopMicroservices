b036d2a84720056f432d63817642e783e78e7fa8
Hezly <32444835+hezly@users.noreply.github.com>
2025-02-27 11:19:06 +0700

    ✨ feat(database): add DiscountContext and initial setup for Discount.Grpc service
    Added `DiscountContext` for managing `Coupon` entities with EF Core.
    Included initial data seeding and migration files for `Coupons` table.
    Updated `Discount.Grpc.csproj` with necessary package references.
    Created `Dockerfile` for gRPC service build and runtime environment.
    Implemented `DiscountService` for gRPC methods handling `Coupon` entities.
    Configured application settings and launch profiles for different environments.
    
    Benefits:
    - Streamlined database operations with EF Core.
    - Simplified service deployment using Docker.
    - Enhanced gRPC service functionality for managing discounts.

---
+++ src/Services/Discount/Discount.Grpc/GlobalUsing.cs
@@ -0,0 +1,4 @@
+﻿global using Discount.Grpc.Data;
+global using Discount.Grpc.Models;
+global using Mapster;
+global using Grpc.Core;
---
+++ src/Services/Discount/Discount.Grpc/Services/DiscountService.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Discount.Grpc.Services;
+
+public class DiscountService(DiscountContext dbContext)
+    : DiscountProtoService.DiscountProtoServiceBase
+{
+    public override async Task<CouponModel> GetDiscount(GetDiscountRequest request, ServerCallContext context)
+    {
+        var coupon = await dbContext.Coupons.FirstOrDefaultAsync(c => c.ProductName == request.ProductName);
+        coupon ??= new Coupon { ProductName = "No Discount", Amount = 0, Description = "No Discount Desc" };
+
+        var couponModel = coupon.Adapt<CouponModel>();
+        return couponModel;
+    }
+
+    public override async Task<CouponModel> CreateDiscount(CreateDiscountRequest request, ServerCallContext context)
+    {
+        var coupon = request.Coupon.Adapt<Coupon>();
+        dbContext.Coupons.Add(coupon);
+        await dbContext.SaveChangesAsync();
+        var couponModel = coupon.Adapt<CouponModel>();
+        return couponModel;
+    }
+
+    public override async Task<CouponModel> UpdateDiscount(UpdateDiscountRequest request, ServerCallContext context)
+    {
+        var coupon = await dbContext.Coupons.FirstOrDefaultAsync(c => c.Id == request.Coupon.Id) ?? 
+            throw new RpcException(new Status(StatusCode.InvalidArgument, "Invalid request"));
+        coupon.ProductName = request.Coupon.ProductName;
+        coupon.Description = request.Coupon.Description;
+        coupon.Amount = request.Coupon.Amount;
+        dbContext.Coupons.Update(coupon);
+        await dbContext.SaveChangesAsync();
+        return coupon.Adapt<CouponModel>();
+    }
+
+    public override async Task<DeleteDiscountResponse> DeleteDiscount(DeleteDiscountRequest request, ServerCallContext context)
+    {
+        var coupon = await dbContext.Coupons.FirstOrDefaultAsync(c => c.ProductName == request.ProductName) ?? throw new RpcException(new Status(StatusCode.NotFound, $"Discount with {request.ProductName} not found"));
+        dbContext.Coupons.Remove(coupon);
+        await dbContext.SaveChangesAsync();
+        return new DeleteDiscountResponse { Success = true };
+    }
+}
